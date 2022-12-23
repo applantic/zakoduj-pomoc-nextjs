47cf929fdafe51d0d97cf153e0e101890beea739
sebastian-jurek <sebastian.jurek44@gmail.com>
2022-12-23 16:56:13 +0100

    Navbar links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export const Header = () => {
-    return (
-      <header>
-        <nav>--nawigacja--</nav>
-      </header>
-    );
-  };
-  
+  return (
+    <header>
+      <nav>
+        <Link href="/">Zakoduj Pomoc</Link>
+        <Link href="/">Projekty</Link>
+        <Link href="/">Zespół</Link>
+        <Link href="/">Dokumenty</Link>
+        <Link href="/">Kontakt</Link>
+      </nav>
+    </header>
+  );
+};
