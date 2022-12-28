248a1751a19cdeefb6ece6794e24830155f1e8d0
sebastian-jurek <sebastian.jurek44@gmail.com>
2022-12-28 21:18:21 +0100

    Header component styles updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,37 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <Link href="/">Zakoduj Pomoc</Link>
-        <Link href="./projects">Projekty</Link>
-        <Link href="./team">Zespół</Link>
-        <Link href="./documents">Dokumenty</Link>
-        <Link href="./contact">Kontakt</Link>
-      </nav>
+    <header className="bg-white shadow-md shadow-slate-300 font-normal mb-5">
+      <div className="flex flex-wrap items-center justify-center my-0 mx-auto h-auto max-w-6xl">
+        <strong className="m-2.5">
+          <Link href="/" className="text-indigo-900 no-underline mt-2.5">
+            Zakoduj Pomoc ❤️
+          </Link>
+        </strong>
+        <nav className="text-indigo-900 my-2.5 mx-auto">
+          <Link
+            href="./projects"
+            className="mt-1 mr-3 mb-1 ml-3 hover:font-bold"
+          >
+            Projekty
+          </Link>
+          <Link href="./team" className="mt-1 mr-3 mb-1 ml-3 hover:font-bold">
+            Zespół
+          </Link>
+          <Link
+            href="./documents"
+            className="mt-1 mr-3 mb-1 ml-3 hover:font-bold"
+          >
+            Dokumenty
+          </Link>
+          <Link
+            href="./contact"
+            className="mt-1 mr-3 mb-1 ml-3 hover:font-bold"
+          >
+            Kontakt
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
