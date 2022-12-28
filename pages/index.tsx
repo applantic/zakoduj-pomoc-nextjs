9ed1fe680e43055c21db364fcd5c185179ca696a
sebastian-jurek <sebastian.jurek44@gmail.com>
2022-12-28 22:24:16 +0100

    home page content added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,57 @@
+import Image from "next/image";
+
 const Home = () => {
-  return <main className="flex-grow">--zawartość--</main>;
+  return (
+    <main className="flex-grow flex justify-center items-end">
+      <section>
+        <div>
+          <h1>Budujemy software dla&nbsp;fundacji charytatywnych</h1>
+          <p>
+            Nasi wolontariusze pracują ciężko, <br />
+            żebyście Wy mogli pracować lżej
+          </p>
+        </div>
+        <Image
+          src="/../public/laptop.png"
+          alt={"Example image"}
+          width={100}
+          height={100}
+        ></Image>
+      </section>
+      <section>
+        <Image
+          src="/../public/calender.png"
+          alt={"Example image"}
+          width={100}
+          height={100}
+        ></Image>
+        <div>
+          <h2>Potrzebujesz Pomocy?</h2>
+          <p>
+            Skontaktuj sie z nami. Opisz nam swój problem i jeśli będziemy w
+            stanie pomóc, umówimy się na spotkanie.
+          </p>
+          <button>klik!</button>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Zobacz nasze projekty.</h2>
+          <p>
+            Do każdego projektu podchodzimy indywidualnie. Skupiamy się na
+            rozwiazywaniu problemów i przejrzystym designie.
+          </p>
+          <button>klik!</button>
+        </div>
+        <Image
+          src="/../public/folder.png"
+          alt={"Example image"}
+          width={100}
+          height={100}
+        ></Image>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
