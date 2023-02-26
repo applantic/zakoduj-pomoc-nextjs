b2f576c468b11d7e47f684a803081695a6278f75
sebastian-jurek <sebastian.jurek44@gmail.com>
2023-02-26 14:10:57 +0100

    external links to articles added

--- content/projectdetails.tsx
+++ content/projectdetails.tsx
@@ -7,7 +7,9 @@ export default [
         description: `[**„Ratuj Z Sercem”**](https://ratujzsercem.pl/ "przejdź na ratujzsercem.pl") to projekt którego celem jest rozpowszechnianie informacji na temat powszechnego dostępu do defibratorów automatycznych i ich użytkowania.  
         Główny feature portalu to **mapa defibratorów**, która ze względu na brak aktualizacji oraz przestarzałość, przestała działać.  
         Mapa została napisana od nowa przez Piotra, dzięki czemu znowu możemy cieszyć się pełną funkcjonalnością portalu.  
-        Obecnie skupiamy się na **redesignie** całego portalu.`,
+        Obecnie skupiamy się na **redesignie** całego portalu.  
+        ---  
+        [_Wywiad z Piotrem Korlaga i dr n. med. Wojciech Rychlik przeprowadzony przez dr Macieja Kaweckiego_](https://fb.watch/9TpRwipMI8/ "przejdź na facebook'a")`,
 
         link:
           "https://fb.watch/9TpRwipMI8/",
@@ -21,7 +23,10 @@ export default [
         Zespół Piotra Korlaga po wygraniu wyzwania, podpisał umowę na stworzył **internetowy system logistyczny**, dzięki któremu fryzjerzy mogli dostarczać
         ścięte włosy bezpośrednio do fundacji Rak'n'Roll.  
         Podczas pierwszego roku działa systemu, do fundacji dotarło ponad **41,000 dotacji** włosów.  
-        Mimo że fundacja Zakoduj Pomoc nie miała wtedy formalnej działalności, uznajemy to za początek naszej organizacji.
+        Mimo że fundacja Zakoduj Pomoc nie miała wtedy formalnej działalności, uznajemy to za początek naszej organizacji.  
+        ---  
+        [_Artykuł na temat projektu [ENG]_](https://outride.rs/en/a-group-of-polish-computer-programmers-want-to-help-teachers-work-remotely/)
+
         `,
         
         link:
@@ -34,7 +39,9 @@ export default [
         image: "/geeks.jpg",
         description: `Podczas pierwszego lockdownu nauczyciele stanęli przed wielkim wyzwaniem- **zdalnym nauczaniem.**  
         Zespół Geeks3Teachers indywidualnie edukował nauczycieli na temat oprogramowania do zdalnego nauczania.  
-        W między czasie Dawid Perdek wziął pod swoje skrzydła trzy ambitne osoby **bez doświadczenia** programistycznego, do budowania strony pozyskującej wolontariuszy i nauczycieli`,
+        W między czasie Dawid Perdek wziął pod swoje skrzydła trzy ambitne osoby **bez doświadczenia** programistycznego, do budowania strony pozyskującej wolontariuszy i nauczycieli  
+        ---  
+        [_Artykuł na temat projektu [ENG]_](https://outride.rs/en/a-group-of-polish-computer-programmers-want-to-help-teachers-work-remotely/)`,
         
         link:
           "https://outride.rs/en/a-group-of-polish-computer-programmers-want-to-help-teachers-work-remotely/",
