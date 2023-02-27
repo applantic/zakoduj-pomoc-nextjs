513820cb7f9cbba9033fc3a1976dad90b2de0cb9
sebastian-jurek <sebastian.jurek44@gmail.com>
2023-02-27 20:42:44 +0100

    tailwind typo fixed

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
       <div className="max-w-5xl">
       <h1 className="text-3xl text-indigo-900 font-semibold my-4 mx-6 sm:mx-0 lg:my-4">Kontakt</h1>
       <p className="text-lg text-indigo-900 my-0 mx-6 md:text-sm sm:mx-0 lg:my-0">Zapraszamy do kontatku na</p>
-      <p className="text-5xl text-indigo-900 my-0 mx-6 md:text-sm sm:mx-0 lg:my-0 underline ">kontakt@zakodujpomoc.pl</p>
+      <p className="text-lg text-indigo-900 my-0 mx-6 md:text-sm sm:mx-0 lg:my-0 underline ">kontakt@zakodujpomoc.pl</p>
       </div>
     </div>
   );
