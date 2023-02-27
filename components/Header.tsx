4f3e6779110daceb5be27382723d0508971d3355
sebastian-jurek <sebastian.jurek44@gmail.com>
2023-02-27 20:40:21 +0100

    navbar active links styling (useRouter)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export const Header = () => {
+
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
   return (
+    
     <header className="bg-white shadow-md shadow-slate-300 font-normal">
       <div className="flex flex-wrap items-center justify-center my-0 mx-auto h-auto max-w-6xl">
         <strong className="m-2.5">
@@ -12,22 +18,22 @@ export const Header = () => {
         <nav className="text-indigo-900 my-2.5 mx-auto">
           <Link
             href="./projects"
-            className="mt-1 mr-3 mb-1 ml-3 hover:underline"
+            className={currentRoute === '/projects' ? "mt-1 mr-3 mb-1 ml-3 hover:underline font-bold" : "mt-1 mr-3 mb-1 ml-3 hover:underline"}
           >
             Projekty
           </Link>
-          <Link href="./team" className="mt-1 mr-3 mb-1 ml-3 hover:underline">
+          <Link href="./team" className={currentRoute === '/team' ? "mt-1 mr-3 mb-1 ml-3 hover:underline font-bold" : "mt-1 mr-3 mb-1 ml-3 hover:underline"}>
             Zespół
           </Link>
           <Link
             href="./documents"
-            className="mt-1 mr-3 mb-1 ml-3 hover:underline"
+            className={currentRoute === '/documents' ? "mt-1 mr-3 mb-1 ml-3 hover:underline font-bold" : "mt-1 mr-3 mb-1 ml-3 hover:underline"}
           >
             Dokumenty
           </Link>
           <Link
             href="./contact"
-            className="mt-1 mr-3 mb-1 ml-3 hover:underline"
+            className={currentRoute === '/contact' ? "mt-1 mr-3 mb-1 ml-3 hover:underline font-bold" : "mt-1 mr-3 mb-1 ml-3 hover:underline"}
           >
             Kontakt
           </Link>
